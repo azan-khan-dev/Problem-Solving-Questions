9ee867b1c560dc5f63c32ba7aaf050543ed4595c
azan-khan-dev <azankhan.connect@gmail.com>
2026-06-25 21:58:32 +0500

    counting word program added

---
+++ Basic/count_word.js
@@ -0,0 +1,16 @@
+function countWords(sentence) {
+    let count = 0;
+    let inWord = false;
+
+    for (let i = 0; i < sentence.length; i++) {
+        if (sentence[i] !== " " && !inWord) {
+            count++;
+            inWord = true;
+        } else if (sentence[i] === " ") {
+            inWord = false;
+        }
+    }
+    return count;
+}
+
+console.log(countWords("I am a backend engineer")); 
