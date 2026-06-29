6b8aac0277ec2763e5708a0cbeeb8ba996274f70
azan-khan-dev <azankhan.connect@gmail.com>
2026-06-29 22:55:50 +0500

    Capitalize each word

---
+++ Basic/Capitalize.js
@@ -0,0 +1,6 @@
+function titleCase(str) {
+  return str.split(" ")
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(" ");
+}
+console.log(titleCase("hello world")); // "Hello World"
