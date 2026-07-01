e4480b819e0432f6d552df877b394769f143fa41
azan-khan-dev <azankhan.connect@gmail.com>
2026-07-01 21:43:06 +0500

    Product

---
+++ Intermediate/product.js
@@ -0,0 +1,20 @@
+function productExceptSelf(nums) {
+  const n = nums.length;
+  const output = new Array(n).fill(1);
+
+  let leftProduct = 1;
+  for (let i = 0; i < n; i++) {
+    output[i] = leftProduct;
+    leftProduct *= nums[i];
+  }
+
+  let rightProduct = 1;
+  for (let i = n - 1; i >= 0; i--) {
+    output[i] *= rightProduct;
+    rightProduct *= nums[i];
+  }
+
+  return output;
+}
+
+console.log(productExceptSelf([1,2,3,4])); // [24,12,8,6]
